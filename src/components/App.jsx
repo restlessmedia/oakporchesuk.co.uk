5acc98c0c467fa189af934e26f855899a7d8fc8e
Phil Cooper <phillycooper@hotmail.com>
2019-06-14 08:56:45 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        App
+        <img src="OakPorch-logo-white-1.0.png" style={{margin: '10% auto', display: 'block'}} />
       </div>
     )
   }
