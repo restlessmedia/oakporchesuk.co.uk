d324460fcccb3d2e8849898826371c5ab0d2f4b9
Phil Cooper <phillycooper@hotmail.com>
2019-09-11 10:06:16 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Component } from 'inferno';
 
-const App = () => {(
-  <div>
-    <img src="build/images/OakPorch-logo-white-1.0.png" style={{margin: '10% auto', display: 'block'}} />
-  </div>
-)};
+const App = () => {
+  return (
+    <div>
+      <img src="build/images/OakPorch-logo-white-1.0.png" style={{margin: '10% auto', display: 'block'}} />
+    </div>
+  )
+};
 
 export default App;
