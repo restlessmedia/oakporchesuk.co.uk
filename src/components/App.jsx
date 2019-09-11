58538a0df7d5c1743ea7fdc8c7926e378548dca0
Phil Cooper <phillycooper@hotmail.com>
2019-09-11 09:16:13 +0200

    Convert to stateless.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,9 @@
 import { Component } from 'inferno';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <img src="build/images/OakPorch-logo-white-1.0.png" style={{margin: '10% auto', display: 'block'}} />
-      </div>
-    )
-  }
-}
+const App = () => {(
+  <div>
+    <img src="build/images/OakPorch-logo-white-1.0.png" style={{margin: '10% auto', display: 'block'}} />
+  </div>
+)};
 
 export default App;
